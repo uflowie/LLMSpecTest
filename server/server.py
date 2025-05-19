60f01011ad3ce53c9290269aaacf07b8aeffb852
uflowie <bananasawsm@gmail.com>
2025-05-19 19:01:55 +0200

    fix readiness

--- server/server.py
+++ server/server.py
@@ -111,5 +111,9 @@ def create_book():
 
     return jsonify(book), 201
 
+@app.route("/")
+def hello():
+    return "Hello, World!", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
